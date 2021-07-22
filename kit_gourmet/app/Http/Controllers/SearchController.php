44882edb3c65870e42846fca1b94ea556b8350dc
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-07-22 19:33:39 +0900

    外部APIへの実装

--- kit_gourmet/app/Http/Controllers/SearchController.php
+++ kit_gourmet/app/Http/Controllers/SearchController.php
@@ -3,10 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class SearchController extends Controller
 {
     public function index(){
         return view("index");
+    }
+
+    // HTTPリクエストを送るURL
+    private const REQUEST_URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/';
+
+    // APIキー
+    private $api_key;
+
+    public function hotpepperSearch(Request $request)
+    {
+        $keyword = $request["search"]["junre"];
+        $range = $request["search"]["range"];
+        $count = $request["search"]["count"];
+
+        // インスタンス生成
+        $client = new Client();
+
+        // HTTPリクエストメソッド
+        $method = 'GET';
+
+        // APIキーを取得
+        $this->api_key = config('hotpepper.api_key');
+
+        // APIキーや検索ワードなどの設定を記述
+        $options = [
+            'query' => [
+                'key' => config('hotpepper.api_key'),
+                'keyword' => $keyword,
+                'range' => $range,
+                'count' => $count,
+                'format' => 'json',
+            ],
+        ];
+
+        // HTTPリクエストを送信
+        $response = $client->request($method, self::REQUEST_URL, $options);
+        // dd($response);
+        // 'format' => 'json'としたのでJSON形式でデータが返ってくるので、連想配列型のオブジェクトに変換
+        $shops = json_decode($response->getBody(), true)['results'];
+
+        // dd($shops);
+        // index.blade.phpを表示する
+        return view('index', compact('shops'));
     }
 }
